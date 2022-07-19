885efd05af2b389f1cde12db80e9b63a40782adf
bilandb <bbilandzi@tvz.hr>
2022-07-19 19:37:05 +0200

    Cleanup

--- src/main/java/com/bbzavrsni/zavrsni/model/pojo/ApplicationUser.java
+++ src/main/java/com/bbzavrsni/zavrsni/model/pojo/ApplicationUser.java
@@ -16,36 +16,4 @@ public class ApplicationUser implements Serializable {
     private String username;
     private String password;
     private List<SimpleGrantedAuthority> authorities;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<SimpleGrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(List<SimpleGrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-
-    public Long getUID() {
-        return UID;
-    }
-
-    public void setUID(Long UID) {
-        this.UID = UID;
-    }
 }
