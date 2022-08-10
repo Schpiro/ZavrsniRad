f0e40f377d9c01259b858e09f34b61d2c6bbd93a
Borna Bilandzija <borna.bilandzija@outlook.com>
2022-08-10 22:18:46 +0200

    Handling if user is not connected

--- src/main/java/com/bbzavrsni/zavrsni/handler/EventHandler.java
+++ src/main/java/com/bbzavrsni/zavrsni/handler/EventHandler.java
@@ -26,7 +26,7 @@ public class EventHandler {
             case PRIVATE_MESSAGE -> sendPrivateMessage(GsonUtil.fromJson(websocketMessageDTO.getPayload().toString(), MessageDTO.class), false);
             case GROUP_MESSAGE -> sendPrivateMessage(GsonUtil.fromJson(websocketMessageDTO.getPayload().toString(), MessageDTO.class), true);
             case NEW_GROUP -> newGroupCreated(GsonUtil.fromJson(websocketMessageDTO.getPayload().toString(), MessageGroupDTO.class));
-            case OFFER,ANSWER,ICE_CANDIDATE, END_CALL-> sendRTCWebSocketMessage(websocketMessageDTO.getPayload(),websocketMessageDTO.getType(),websocketMessageDTO.getRecipientIds(),websocketMessageDTO.getSenderId());
+            case OFFER, ANSWER,ICE_CANDIDATE, END_CALL-> sendRTCWebSocketMessage(websocketMessageDTO.getPayload(),websocketMessageDTO.getType(),websocketMessageDTO.getRecipientIds(),websocketMessageDTO.getSenderId());
             case NEW_EVENT -> sendWebSocketMessage(GsonUtil.fromJson(websocketMessageDTO.getPayload().toString(), EventDTO.class),MessageTypes.NEW_EVENT,openSessions.keySet().stream().toList());
             case NEW_COMMENT -> sendWebSocketMessage(GsonUtil.fromJson(websocketMessageDTO.getPayload().toString(), CommentDTO.class),MessageTypes.NEW_COMMENT,openSessions.keySet().stream().toList());
         }
@@ -36,6 +36,9 @@ public class EventHandler {
         for (Long userId : userIds) {
             if (openSessions.containsKey(userId))
                 openSessions.get(userId).sendMessage(new TextMessage(new WebsocketMessageDTO(messageTypes, message, senderId).toString()));
+            else
+                //Mozda poslati novi messagetype da handlea ako netko nije online?
+                openSessions.get(senderId).sendMessage(new TextMessage(new WebsocketMessageDTO(MessageTypes.END_CALL,"User is not online",Long.valueOf(0)).toString()));
         }
     }
 
